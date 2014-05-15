bfd85d62b5d19ab5ea66a8640e70796b8da9f435
speccaN <ericlindeberg@hotmail.com>
2014-05-15 14:54:59 +0200

    Jajaj

--- footer.php
+++ footer.php
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
+
 <a href="http://www.google.se" id="infoBtn">Om sidan</a>
 <a href="" id="reglerBtn">Regler</a>
-=======
-<?php echo "Mamsdmasmdsam"; ?>
->>>>>>> 7d94d50301d2ab7cd05b6e60ac9e3318ba86ec56
