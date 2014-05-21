cbf543783fdf892b3ea67c8fc39007dcc84bf73c
TimMelker <tim@webbappkonsult.sw>
2014-05-21 12:13:31 +0200

    new
    new

---
+++ registerBusiness.php
@@ -0,0 +1,11 @@
+<!DOCTYPTE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+	<body>
+		<!--Företagsnamn, e-post, telefon, lösenord-->
+		
+	</body>
+</html>
