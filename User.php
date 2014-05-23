1c8a95748eb1d7e2a3a8d8355a5d5d1603462ac9
TimMelker <tim@webbappkonsult.sw>
2014-05-23 15:34:27 +0200

    startingUser
    User

---
+++ User.php
@@ -0,0 +1,18 @@
+<DOCTYPE! html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="userContainer">
+			<div id="profileImage"><!--Lägg in bild från databasen-->
+			</div>
+			<div id="userInfo"><!--Samtliga fält från databasen skall in-->
+
+				<button title="Redigera" name="Redigera" onclick="window.location.href='editUser.php'">Redigera</button>
+
+
+			</div>
+		</div>
+	</body>
+</html>
---
+++ editUser.php
@@ -0,0 +1,18 @@
+<DOCTYPE! html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="userContainer">
+			<div id="profileImage"><!--Lägg in bild från databasen-->
+			</div>
+			<div id="userInfo"><!--Samtliga fält från databasen skall in-->
+
+				<button>Spara</button>
+
+
+			</div>
+		</div>
+	</body>
+</html>
